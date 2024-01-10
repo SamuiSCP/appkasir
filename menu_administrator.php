94b9d57b40a42e1d3fba112fe32d538e74cd3ccd
SamuiSCP <brutuzjoss@gmail.com>
2024-01-10 12:32:30 -0800

    Menambahkan Pengaturan Hak Akses

--- menu_administrator.php
+++ menu_administrator.php
@@ -33,7 +33,7 @@
     <a href="" class="nav-link">
         <i class="nav-icon fas fa-file"></i>
         <p>
-            Penjualan
+            Penjualana
             <i class="right fas fa-angle-left"></i>
         </p>
     </a>
